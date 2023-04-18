8d10e1f1c89573ba1beccd2aed6f8428dbb7e859
Adam Vinch <jjavinch@me.com>
2023-04-18 09:18:41 -0400

    Fixing project structure

--- src/sentry/Sentry.java
+++ src/sentry/Sentry.java
@@ -4,10 +4,13 @@ import sentry.UI.Login;
 import sentry.UI.ResourceLoader;
 import sentry.backend.SQLite;
 
+import java.util.HashMap;
+
 public class Sentry {
   public static void main(String[] args) {
     ResourceLoader.load_resources();
     SQLite.setup();
     new Login();
   }
+  
 }
